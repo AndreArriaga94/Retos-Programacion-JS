5c41503fb5534694b1cfec7ecf390232dd502cbb
AndreArriaga94 <arriaga109407@gmail.com>
2024-02-24 22:59:34 -0600

    reto 10 tenia la informacion del reto 9 se actualiza el ejercicio para iniciar el desarrollo

--- reto10.js
+++ reto10.js
@@ -1,37 +1,16 @@
 /*
- * Crea un programa se encargue de transformar un número
- * decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
+ * Crea un programa que sea capaz de transformar texto natural a código
+ * morse y viceversa.
+ * - Debe detectar automáticamente de qué tipo se trata y realizar
+ *   la conversión.
+ * - En morse se soporta raya "—", punto ".", un espacio " " entre letras
+ *   o símbolos y dos espacios entre palabras "  ".
+ * - El alfabeto morse soportado será el mostrado en
+ *   https://es.wikipedia.org/wiki/Código_morse.
  */
 
 function fntReto10(){
     
-    let intDecimal = 21
-    let intBinario = ''
-
-    let boolContinuar = true
-
-    do{
-        let intResiduo = intDecimal % 2
-        intBinario += intResiduo.toString()        
-        intDecimal = parseInt(intDecimal / 2)
-        boolContinuar =  intDecimal > 0 ? true : false
-    }while(boolContinuar)
-
-    fntReto7(intBinario);
-
-}
-
-function fntReto7(strBinario){
-
-    let strPalabra = strBinario
-    let arrPalabra = strPalabra.split('')
-    let strPalabraInversa = ''
-
-    for (i = (arrPalabra.length -1); i != -1; i--) { 
-        strPalabraInversa += arrPalabra[i]
-    }
-    
-    console.log(strPalabraInversa)
 }
 
 fntReto10(); 
