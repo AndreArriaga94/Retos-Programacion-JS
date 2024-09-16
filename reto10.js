3e80a9fbeea15c30f4d81f98481fd1e6533d26ac
Ismar André Arriaga Lemus <arriaga109407@gmail.com>
2024-09-15 22:13:11 -0600

    se resuelve el reto 10 de clave morse

--- reto10.js
+++ reto10.js
@@ -9,8 +9,63 @@
  *   https://es.wikipedia.org/wiki/Código_morse.
  */
 
+const objLetras = {
+    A: ' . - ',
+    B: ' - . . . ',
+    C: ' - . - . ',
+    D: ' - . . ',
+    E: ' . ',
+    F: ' . . - . ',
+    G: ' - - . ',
+    H: ' . . . . ',
+    I: ' . . ',
+    J: ' . - - - ',
+    K: ' - . - ',
+    L: ' . - . . ',
+    M: ' - - ',
+    N: ' - . ',
+    O: ' - - - ',
+    P: ' . - - . ',
+    Q: ' - - . - ',
+    R: ' . - . ',
+    S: ' . . . ',
+    T: ' - ',
+    U: ' . . - ',
+    V: ' . . . - ',
+    W: ' . - - ',
+    X: ' - . . - ',
+    Y: ' - . - - ',
+    Z: ' - - . . ',
+    1: ' . - - - - ',
+    2: ' . . - - - ',
+    3: ' . . . - - ',
+    4: ' . . . . - ',
+    5: ' . . . . . ',
+    6: ' - . . . . ',
+    7: ' - - . . . ',
+    8: ' - - - . . ',
+    9: ' - - - - . ',
+    0: ' - - - - - ',
+}
+
+const objMorse = Object.fromEntries(
+    Object.entries(objLetras).map(([key, value]) => [value.trim(), key])
+)
+
 function fntReto10(){
-    
+    let strPalabra = '123';
+    let strMorse = '. -  - - ';
+
+    let arrLetras = strPalabra.toUpperCase().split('')
+    let arrMorse = strMorse.split('  ')
+
+    for (let i in arrLetras){
+        console.log(objLetras[i].trim(),i)
+    }
+
+    for (let j in arrMorse){
+        console.log(objMorse[arrMorse[j].trim()],arrMorse[j])
+    }
 }
 
 fntReto10(); 
