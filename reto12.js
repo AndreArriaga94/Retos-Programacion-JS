401b3bf6e3cdb974df394bed533851f717c1f7f3
Ismar André Arriaga Lemus <arriaga109407@gmail.com>
2024-09-25 22:00:18 -0600

    ser resuelve el reto12

--- reto12.js
+++ reto12.js
@@ -8,5 +8,48 @@
  */
 
 function fntReto12(){
+    let str1,str2,out1 = out2 = ''
+    let arrStr1, arrStr2 = []
+    let boolExiste1, boolExiste2 = false
 
+    str1 = 'aeio'
+    str2 = 'au'
+
+    arrStr1 = str1.split('')
+    arrStr2 = str2.split('')
+
+    for (let i in arrStr1){
+        
+        for (let j in arrStr2){
+            if(arrStr2[j] == arrStr1[i]){
+                boolExiste1 = true
+            }
+        }
+
+        if(!boolExiste1){
+            out1 += ' ' + arrStr1[i]
+        }
+
+        boolExiste1 = false
+    }
+
+    for (let k in arrStr2){
+        
+        for (let l in arrStr1){
+            if(arrStr2[k] == arrStr1[l]){
+                boolExiste2 = true
+            }
+        }
+
+        if(!boolExiste2){
+            out2 = ' ' + arrStr2[k]
+        }
+
+        boolExiste2 = false
+    }
+
+    console.log(out1)
+    console.log(out2)
 }
+
+fntReto12()
